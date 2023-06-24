b7f6ddf1b25db51774c8b10d5235359f08dd489c
sahan madusha <74488190+sahan-madusha@users.noreply.github.com>
2023-06-25 00:13:01 +0530

    Update geo.js

--- API/geo.js
+++ API/geo.js
@@ -4,7 +4,7 @@ const axios = require('axios'); // legacy way
 
 const getGeo = (location) => {
   return new Promise((resolve, reject) => {
-    const groUrl = "http://api.openweathermap.org/geo/1.0/direct?q="+location+"&limit=5&appid=51c4effde82e365e50485659258740ae";
+    const groUrl = "http://api.openweathermap.org/geo/1.0/direct?q="+location+"&limit=5&appid=";
 
     // Make a request for a user with a given ID
     axios.get(groUrl)
